90da1a5decccd2e8e274c912686b0d10d92e0dee
FELIPPE DE PAULA DUARTE <felippe.pduarte@senacsp.edu.br>
2021-11-12 11:35:23 -0300

    projeto

--- App.tsx
+++ App.tsx
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { estilo } from './css/Styles';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+  <View>
+    {/* Inicio menu do cabeçalho.  */}
+    <View style={estilo.menu}>
+
+      <Text style={estilo.search}>
+        Search
+        </Text>
+      
+      <Image source={require("./assets/menu.png")} style={estilo.icone}/>
+      {/* Fim do cabeçalho */}
+
+      {/* Imagem de destaque */}
+      <Image source={{ uri:"https://thumbs.dreamstime.com/b/teacher-books-helping-student-online-lesson-laptop-screen-tutor-demand-homework-help-english-concept-pinkish-coral-162327294.jpg"}} style={estilo.image}/>
+      {/* Fim imagem destaque */}
+
+      {/* Começo da area de usuario */}
+      <View style={estilo.fundo}>
+
+        <Image source={require('./assets/user.png')} style={estilo.user}/>
+
+        <View style={estilo.texto}>
+            <Text style={estilo.textuser}>
+              Paulo anotonio
+            </Text>
+
+            <Text style={estilo.textuser2}>
+              São Paulo
+            </Text>
+        </View>
+
+        <View style={estilo.avaliacao}>
+
+          <Image source={{uri:""}} style={estilo.estrela}/>
+          <Text style={estilo.valor}>
+              $ 230/h
+          </Text>
+
+        </View>
+      </View>
+   
+
+      </View>
+  </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
---
+++ css/Styles.tsx
@@ -0,0 +1,52 @@
+import { StyleSheet } from "react-native";
+
+export const estilo = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'#090909' 
+},
+
+menu:{
+backgroundColor:'#94CDAA'
+},
+
+    search:{
+
+    },
+
+    icone:{
+
+    },
+    image:{
+        width:370,
+        height:200
+    },
+    fundo:{
+
+    },
+    user:{
+        margin:10,
+        width:100,
+        height:100
+
+    },
+    texto:{
+
+    },
+    textuser:{
+
+    },
+    textuser2:{
+
+    },
+    avaliacao:{
+
+    },
+    estrela:{
+
+    },
+    valor:{
+
+}
+
+})
